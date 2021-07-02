c692e2d342cbaa7f1fac58b545aac5c89efbb4ff
Daniela Maria <daniela17maria@gmail.com>
2021-07-02 15:55:27 -0300

    meu projeto

---
+++ scriptIndex.js
@@ -0,0 +1,17 @@
+/* var nome = window.prompt('Qual seu nome?')
+window.alert('É um prazer te conhecer ' + nome + '!' + ' Fico agradecida por ter curiosidade sobre minha página .')
+
+ window.confirm('Está gostando?')
+ window.prompt('qual seu nome?')
+
+var n1 = Number.parseInt(window.prompt('Digite um numero:'))
+var n2 = Number.parseInt(window.prompt('Digite um numero:'))
+var s = n1 + n2
+window.alert('A soma dos valores é: ' + s)
+ 
+var nome = window.prompt('Qual seu nome?')
+document.write(`Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras.<br>`)
+document.write(`Seu nome em maiusculas é ${nome.toUpperCase()} .<br>`)
+document.write(`Seu nome em minusculas é ${nome.toLocaleLowerCase()} .<br>`)*/
+
+console.log(' funcionou!')
